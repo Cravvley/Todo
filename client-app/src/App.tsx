d3161d47227a2972bcae1f63baefa28a3080e090
daniel niekurzak <danielniekurzak@gmail.com>
2024-06-17 10:29:24 +0200

    add client app

---
+++ client-app/src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import { List } from "semantic-ui-react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/TodoItems").then((response) => {
+      setTodos(response.data);
+    });
+  });
+  return (
+    <>
+      <List>
+        {todos.map((todo: any) => (
+          <List.Item key={todo.id}>{todo.title}</List.Item>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
