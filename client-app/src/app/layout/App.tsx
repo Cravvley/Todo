9a741a92d2fa4e6174627b1fd656338bb4b97244
daniel niekurzak <danielniekurzak@gmail.com>
2024-06-19 13:23:39 +0200

    add todoitem interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { List } from "semantic-ui-react";
+import { TodoItem } from "../models/todoItem";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<TodoItem[]>([]);
 
   useEffect(() => {
     axios.get("http://localhost:5000/TodoItems").then((response) => {
@@ -13,7 +14,7 @@ function App() {
   return (
     <>
       <List>
-        {todos.map((todo: any) => (
+        {todos.map((todo: TodoItem) => (
           <List.Item key={todo.id}>{todo.title}</List.Item>
         ))}
       </List>
